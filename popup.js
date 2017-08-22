e22f80a887f4fc574a081f10490b8821f972457b
Lin <zzlyn321@hotmail.com>
2017-08-22 12:46:54 -0400

    Added a http get request, expect result from java analyzer service.

--- popup.js
+++ popup.js
@@ -1,5 +1,18 @@
 chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
    function(tabs){
-      alert(tabs[0].url);
+	  document.getElementById('urldiv').innerHTML = tabs[0].url.toString();
+      setResult();
    }
 );
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+function setResult(){
+    document.getElementById('response').innerHTML = httpGet("http://localhost:8000/analyze");
+}
