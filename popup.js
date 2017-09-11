0c5402435af2e4acb0f2e021f135f32939ade52b
Lin <zzlyn321@hotmail.com>
2017-09-11 15:56:36 -0400

    Added regex function to check if current url is of reddit discussion. Rejects request from front end if its invalid url.

--- popup.js
+++ popup.js
@@ -1,11 +1,11 @@
-var taburi;
+var postRegex = new RegExp("https:\/\/www.reddit.com\/r\/.*?[\/]comments\/.*");
 
 chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
    function(tabs){
 	  document.getElementById('urldiv').innerHTML = tabs[0].url.toString();
-	  taburi = tabs[0].url.toString();
+	  var taburi = tabs[0].url.toString();
 
-	  setResult();
+	  setResult(taburi);
    }
 );
 
@@ -20,6 +20,13 @@ function httpGet(reqUrl)
     return xmlHttp.responseText;
 }
 
-function setResult(){
-    document.getElementById('response').innerHTML = httpGet(taburi);
+function setResult(taburi){
+	if(postRegex.test(taburi) == true){
+		document.getElementById('response').innerHTML = "this is a reddit post address, can be analyzed";
+		// document.getElementById('response').innerHTML = httpGet(taburi);
+	}else{
+		document.getElementById('response').innerHTML = "This is not a reddit post!";
+	}
+	
+    // document.getElementById('response').innerHTML = httpGet(taburi);
 }
